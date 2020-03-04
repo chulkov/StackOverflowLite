d5b55196caf3338e0ae2570194f97213bb2576da
chulkov <oleg.ponomarev@rasolution.ru>
2020-03-04 13:41:59 +0500

    Initial Commit

---
+++ StackOverflowLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackOverflowLite
+//
+//  Created by Max on 04/03/2020.
+//  Copyright © 2020 chulkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
