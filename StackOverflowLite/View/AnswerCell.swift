4eeac90e8498026f983a1c82374fd709f730c78c
chulkov <oleg.ponomarev@rasolution.ru>
2020-03-05 14:05:05 +0500

    detailView tableview with 2 static and 1 dynamic cells

---
+++ StackOverflowLite/View/AnswerCell.swift
@@ -0,0 +1,24 @@
+//
+//  AnswerCell.swift
+//  StackOverflowLite
+//
+//  Created by Max on 05/03/2020.
+//  Copyright © 2020 chulkov. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- StackOverflowLite/View/DetailViewController.swift
+++ StackOverflowLite/View/DetailViewController.swift
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UITableViewController {
     
     @IBOutlet weak var testLabel: UILabel!
     public var viewModel: QuestionsColletionViewCellModel?
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,6 +21,46 @@ class DetailViewController: UIViewController {
             testLabel.text = viewModel.title
         }
         
+        tableView.register(UINib(nibName: "AnswerCell", bundle: nil), forCellReuseIdentifier: "AnswerCell")
+        
     }
-
+    
+    
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 2
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+                if section == 1 {
+            return 5
+            //the datasource of the dynamic section
+        }
+        return super.tableView(tableView, numberOfRowsInSection: section)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AnswerCell") as! AnswerCell
+            return cell
+        }
+        return super.tableView(tableView, cellForRowAt: indexPath)
+    }
+    
+    override func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+                if indexPath.section == 1 {
+            let newIndexPath = IndexPath(row: 0, section: indexPath.section)
+            return super.tableView(tableView, indentationLevelForRowAt: newIndexPath)
+        }
+        return super.tableView(tableView, indentationLevelForRowAt: indexPath )
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 1 {
+            return 44
+        }
+        return super.tableView(tableView, heightForRowAt: indexPath)
+    }
 }
